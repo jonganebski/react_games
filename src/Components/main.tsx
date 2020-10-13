e6bd4c2a4d49bb75c440263b8c736067dca3ac8a
Jon Ganebski <jon.ganebski@gmail.com>
2020-10-13 23:25:46 +0900

    Made some basic stuff

--- src/Components/main.tsx
+++ src/Components/main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -18,15 +18,54 @@ const MineBox = styled.div`
 
 // 9x9 16x16 30X16
 // 10   40    99
+const easy = { size: { x: 9, y: 9 }, level: "easy" };
+const midd = { size: { x: 16, y: 16 }, level: "moderate" };
+const hard = { size: { x: 30, y: 16 }, level: "hard" };
+
 const Main = () => {
-  const x = 30;
-  const y = 16;
+  const [size, setSize] = useState(midd.size);
   return (
-    <Container style={{ gridTemplateColumns: `repeat(${x}, 1fr)` }}>
-      {Array.from(Array(x * y).keys()).map((_, i) => (
-        <MineBox key={i} />
-      ))}
-    </Container>
+    <>
+      <Container style={{ gridTemplateColumns: `repeat(${size.x}, 1fr)` }}>
+        {Array.from(Array(size.x * size.y).keys()).map((_, i) => (
+          <MineBox
+            key={i}
+            onMouseDown={() => console.log("mouse down")}
+            onMouseUp={() => console.log("mouse up ")}
+            onMouseEnter={(e) => {
+              if (e.buttons === 1) {
+                console.log("you are draging");
+              } else {
+                return;
+              }
+            }}
+            onMouseLeave={(e) => {
+              if (e.buttons === 1) {
+                console.log("you are draging");
+              } else {
+                return;
+              }
+            }}
+          />
+        ))}
+      </Container>
+      <select
+        defaultValue={midd.level}
+        onChange={(e) => {
+          if (e.currentTarget.value === easy.level) {
+            setSize(easy.size);
+          } else if (e.currentTarget.value === midd.level) {
+            setSize(midd.size);
+          } else {
+            setSize(hard.size);
+          }
+        }}
+      >
+        <option value={easy.level}>Easy</option>
+        <option value={midd.level}>Moderate</option>
+        <option value={hard.level}>Hard</option>
+      </select>
+    </>
   );
 };
 
