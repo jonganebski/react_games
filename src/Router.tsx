e73fb98158d3453c874cf356c5920d19df712040
Jon Ganebski <jon.ganebski@gmail.com>
2020-10-18 18:50:06 +0900

    Sudoku generate. (deep copy for template)

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import Home from "./Components/Home";
 import Minesweeper from "./Components/Minesweeper";
+import Sudoku from "./Components/Sudoku/Sudoku";
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/minesweeper" component={Minesweeper} />
+        <Route path="/sudoku" component={Sudoku} />
       </Switch>
     </HashRouter>
   );
